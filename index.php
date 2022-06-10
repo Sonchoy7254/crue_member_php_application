99837538a6ca57aa13cadd997605c74fbcf2cb11
sonchoy <sonchoy7254@gmail.com>
2022-06-10 21:51:12 +0600

    Read the json file, and show it on the fontned

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
-<?php include "inc/header.php";?>
+<?php require "inc/header.php";?>
+<?php require "users.php"; 
+$users = getUsers();
+?>
 
 
 
 
+<table class="table">
+    <thead>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Website</th>
+    </thead>
+    <tbody>
+        <?php foreach($users as $user):?>
 
-<?php include "inc/footer.php" ?>
+            <tr>
+                    <td><?php  echo $user['name'] ?></td>
+                    <td><?php  echo $user['age']; ?></td>   
+                    <td><?php  echo $user['email']; ?></td>
+                    <td><?php  echo $user['password']; ?></td>
+                    <td><?php  echo $user['website']; ?></td>
+                
+            </tr>
+
+            <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+
+
+<!-- Footer file adding  -->
+<?php require "inc/footer.php" ?>
---
+++ users.php
@@ -0,0 +1,19 @@
+<?php
+function getUsers() {
+
+    return json_decode(file_get_contents('users.json'), true);
+   
+ }
+ function getUserById($Id) {
+ 
+ }
+ function createuser($data) {
+ 
+ }
+ function UpdateUser ($data, $Id) {
+ 
+ }
+ function deleteuser($id) {
+ 
+ }
+?>
