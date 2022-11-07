0073e2306ca11a9aa48d7a53fa37c19a6c5a44e2
Hamid Mia Sõñchõy <64643100+Sonchoy7254@users.noreply.github.com>
2022-11-08 03:29:40 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require "init/header.php"; ?>
+
+<?php
+require "users/function.php";
+
+$users = getusers();
+
+
+require "init/home.php";
+
+?>
+
+
+
+
+
+
+
+<?php require "init/footer.php"; ?>
+    
